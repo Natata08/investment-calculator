18de1f2b162237fc70ff2bd76b14789be9282aa6
Natalia Lapina <lapinann08@gmail.com>
2024-09-10 15:35:04 +0200

    add Header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+export default function Header() {
+  return (
+    <header id='header'>
+      <img
+        src='investment-calculator-logo.png'
+        alt='logo of Investment Calculator'
+      />
+      <h1>Investment Calculator</h1>
+    </header>
+  );
+}
